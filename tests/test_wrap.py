fc1adb54bb0a64f91b0714d00d9a0929d66e21e1
Jonathan Herrera <jonathan.herrera@posteo.de>
2024-12-02 23:31:00 +0100

    Add test for non-filling behavior when using linebreak as paragraph marker

--- tests/test_wrap.py
+++ tests/test_wrap.py
@@ -48,3 +48,28 @@ class TestWrap(TestCase):
         assert all(len(line) <= target_width for line in expected_lines)
         expected = "\n".join(expected_lines)
         self.assert_equal(result, expected)
+
+    def test_does_not_fill_when_paragraph_marker_is_linebreak(self):
+        target_width = 64
+        paragraph_marker = "\n"
+        text = (
+            "This is a paragraph."
+            f"{paragraph_marker}"  # paragraph marker should result in paragraph
+            "Here comes a second paragraph, but this one is a little longer"
+            " and it contains a linebreak\n"
+            "which should remain in the output. And this text is just for "
+            "checking linebreaking."
+        )
+        result = wrap(text, width=target_width, paragraph=paragraph_marker)
+        expected_lines = [
+            "This is a paragraph.",
+            "",
+            "Here comes a second paragraph, but this one is a little longer",
+            "and it contains a linebreak",
+            "",
+            "which should remain in the output. And this text is just for",
+            "checking linebreaking.",
+        ]
+        assert all(len(line) <= target_width for line in expected_lines)
+        expected = "\n".join(expected_lines)
+        self.assert_equal(result, expected)

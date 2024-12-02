5c88a18b0d8ee8bb911453cb14cc8c25a85bf11b
Jonathan Herrera <jonathan.herrera@posteo.de>
2024-12-02 01:05:04 +0100

    Formatting

--- tamal/wrap.py
+++ tamal/wrap.py
@@ -1,8 +1,10 @@
 from typing import TypeAlias
+
 from .defaults import DEFAULT_HYPHEN, DEFAULT_HYPHENS, DEFAULT_SOFT_HYPHEN
 
 Head: TypeAlias = str
 Tail: TypeAlias = str
+
 
 def break_text(
     text: str,
@@ -14,13 +16,13 @@ def break_text(
     if len(text) <= width:
         return text, ""
 
-    for (reverse_index, char) in enumerate(reversed(text[:width])):
+    for reverse_index, char in enumerate(reversed(text[:width])):
         index = width - reverse_index
         if char == " ":
-            return text[:index-1], text[index:]
+            return text[: index - 1], text[index:]
         if char in hyphens:
             return text[:index], text[index:]
         if char == soft_hyphen:
-            return text[:index-1] + hyphen, text[index:]
+            return text[: index - 1] + hyphen, text[index:]
 
-    return (text[:width-1] + hyphen, text[width-1:])
+    return (text[: width - 1] + hyphen, text[width - 1 :])

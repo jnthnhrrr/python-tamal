39448e4aa33c2c4f2cb3daf8377d9265f8c66316
Jonathan Herrera <jonathan.herrera@posteo.de>
2024-12-02 01:04:38 +0100

    Test break_text to ignore soft hyphens when considering text length

--- tests/test_breaking.py
+++ tests/test_breaking.py
@@ -2,6 +2,7 @@ from uneedtest import TestCase
 
 from tamal.wrap import break_text
 
+
 class TestBreakText(TestCase):
 
     def test_does_not_break_when_not_necessary(self):
@@ -31,11 +32,23 @@ class TestBreakText(TestCase):
     def test_uses_existing_soft_hyphen(self):
         text = "A line breaking ex·perience"
         result = break_text(text, 21)
-        expected = ("A line breaking ex-" , "perience")
+        expected = ("A line breaking ex-", "perience")
         self.assert_equal(result, expected)
 
     def test_uses_custom_hyphenation_character(self):
         text = "SomeLongWord"
         result = break_text(text, 5, hyphen="~")
         expected = ("Some~", "LongWord")
+        self.assert_equal(result, expected)
+
+    def test_does_not_count_soft_hyphens_against_full_width(self):
+        text = "~~~~~Hello"
+        result = break_text(text, 5, soft_hyphen="~")
+        expected = ("~~~~~Hello", "")
+        self.assert_equal(result, expected)
+
+    def test_does_not_count_soft_hyphens_when_breaking_at_index(self):
+        text = "~~~~~He~llo"
+        result = break_text(text, 3, soft_hyphen="~")
+        expected = ("~~~~~He-", "llo")
         self.assert_equal(result, expected)

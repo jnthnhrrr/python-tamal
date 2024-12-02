bb3f2d0629a60bb92224dd7d021d95ba7f40d95b
Jonathan Herrera <jonathan.herrera@posteo.de>
2024-12-02 04:46:33 +0100

    Test break_text breakes at multichar hyphen

--- tests/test_breaking.py
+++ tests/test_breaking.py
@@ -11,13 +11,13 @@ class TestBreakText(TestCase):
         expected = ("A line breaking experience", "")
         self.assert_equal(result, expected)
 
-    def test_uses_whitespace(self):
+    def test_breaks_at_whitespace_and_removes_leading_whitespace(self):
         text = "A line breaking experience"
         result = break_text(text, 11)
         expected = ("A line", "breaking experience")
         self.assert_equal(result, expected)
 
-    def test_uses_existing_hyphen(self):
+    def test_breaks_at_existing_hyphen(self):
         text = "A line-breaking experience"
         result = break_text(text, 11)
         expected = ("A line-", "breaking experience")
@@ -29,32 +29,38 @@ class TestBreakText(TestCase):
         expected = ("Some-", "LongWord")
         self.assert_equal(result, expected)
 
-    def test_uses_existing_soft_hyphen(self):
+    def test_breaks_at_existing_soft_hyphen(self):
         text = "A line breaking ex·perience"
         result = break_text(text, 21)
         expected = ("A line breaking ex-", "perience")
         self.assert_equal(result, expected)
 
-    def test_uses_custom_hyphenation_character(self):
+    def test_breaks_at_custom_hyphenation_character(self):
         text = "SomeLongWord"
-        result = break_text(text, 5, hyphen="~")
-        expected = ("Some~", "LongWord")
+        result = break_text(text, 5, hyphen="0")
+        expected = ("Some0", "LongWord")
         self.assert_equal(result, expected)
 
     def test_does_not_count_soft_hyphens_against_full_width(self):
-        text = "~~~~~Hello"
-        result = break_text(text, 5, soft_hyphen="~")
-        expected = ("~~~~~Hello", "")
+        text = "·····Hello"
+        result = break_text(text, 5)
+        expected = ("·····Hello", "")
         self.assert_equal(result, expected)
 
     def test_does_not_count_soft_hyphens_when_breaking_at_index(self):
-        text = "~~~~~He~llo"
-        result = break_text(text, 3, soft_hyphen="~")
-        expected = ("~~~~~He-", "llo")
+        text = "·····He·llo"
+        result = break_text(text, 3)
+        expected = ("·····He-", "llo")
         self.assert_equal(result, expected)
 
-    def test_breaks_at_multichar_soft_hypen(self):
-        text = "He~~llo"
-        result = break_text(text, 3, soft_hyphen="~~")
+    def test_breaks_at_multichar_soft_hyphen(self):
+        text = "He··llo"
+        result = break_text(text, 3, soft_hyphen="··")
         expected = ("He-", "llo")
+        self.assert_equal(result, expected)
+
+    def test_breaks_at_multichar_hyphen(self):
+        text = "He--llo"
+        result = break_text(text, 3)
+        expected = ("He--", "llo")
         self.assert_equal(result, expected)
